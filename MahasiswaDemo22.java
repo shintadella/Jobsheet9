182643456da0bbe8e60e6c8c5f72be678bf8775c
shintadella <vaizatusdella@gmail.com>
2025-04-30 15:38:39 +0700

    ini file della

--- MahasiswaDemo22.java
+++ MahasiswaDemo22.java
@@ -12,6 +12,8 @@ public class MahasiswaDemo22 {
             System.out.println("2. Menilai Tugas");
             System.out.println("3. Melihat Tugas Teratas");
             System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("5. Melihat Tugas Terbawah");
+            System.out.println("6. Hitung Jumlah Tugas");
             System.out.print("Pilih: ");
             pilih = scan.nextInt();
             scan.nextLine(); 
@@ -48,9 +50,19 @@ public class MahasiswaDemo22 {
                     System.out.println("Nama\tNIM\tKelas");
                     stack.print();
                     break;
+                case 5:
+                    Mahasiswa22 terbawah = stack.lihatTugasTerbawah();
+                    if (terbawah != null) {
+                        System.out.println("Tugas pertama dikumpulkan oleh:");
+                        System.out.printf("Nama : %s\nNIM  : %s\nKelas: %s\n", terbawah.nama, terbawah.nim, terbawah.kelas);
+                    }
+                    break;
+                case 6:
+                    System.out.println("Jumlah tugas saat ini: " + stack.jumlahTugas());
+                    break;
                 default:
                     System.out.println("Pilihan tidak valid.");
             }
-        } while (pilih >= 1 && pilih <= 4);
+        } while (pilih >= 1 && pilih <= 5);
     }
 }
--- StackTugasMahasiswa22.java
+++ StackTugasMahasiswa22.java
@@ -54,10 +54,23 @@ public class StackTugasMahasiswa22 {
         }
     }
 
+    public Mahasiswa22 lihatTugasTerbawah() {
+        if (!isEmpty()) {
+            return stack[0]; 
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas di dasar stack.");
+            return null;
+        }
+    }
+
+    public int jumlahTugas() {
+        return top + 1;
+    }    
+
     public void print() {
-        for (int i = 0; i <= top; i++) {
-            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
-        }            
+        for (int i = top; i >= 0; i--) {
+            System.out.printf("%s\t%s\t%s\n", stack[i].nama, stack[i].nim, stack[i].kelas);
+        }               
             System.out.println("");
         }
     }
